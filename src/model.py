79aa143332c017ed6afc38f602b94dd1116dc4a1
Suryansh Gupta <suryanshgupta9933@gmail.com>
2023-08-17 01:46:39 +0530

    model and training

--- src/model.py
+++ src/model.py
@@ -2,7 +2,64 @@
 import logging
 from abc import ABC, abstractmethod
 
+from catboost import CatBoostRegressor, Pool
+from lightgbm import LGBMRegressor, LGBMClassifier
 
+# Create a class for the Model
 class Model(ABC):
     """
-    
+    Abstract class for all models
+    """
+    @abstractmethod
+    def train(self, X_train, y_train):
+        """
+        Trains the model
+        Args:
+            X_train (pd.DataFrame): Training data
+            y_train (pd.DataFrame): Training labels
+        Returns:
+            None
+        """
+        pass
+
+class BoostingModels(Model):
+    """
+    Abstract class for all boosting models
+    """
+    def train(self, X_train, y_train, **kwargs):
+        """
+        Trains the model
+        Args:
+            X_train (pd.DataFrame): Training data
+            y_train (pd.DataFrame): Training labels
+        Returns:
+            None
+        """
+        try:
+            # Empty list for ensembling models
+            model = []
+
+            # CatBoostRegressor
+            # Create a pool for training data
+            pool_train = Pool(X_train, y_train)
+            pool_val = Pool(X_val, y_val)
+
+            # Train the model
+            cbr = CatBoostRegressor(iterations = 200)
+            cbr.fit(pool_train, eval_set =(X_val, y_val), verbose=False)
+            
+            # Append the model to the list
+            model.append(cbr)
+
+            #LightGBM
+            # Train the model
+            lgbr = LGBMRegressor(n_estimators = 200)
+            lgbr.fit(X_train, y_train, eval_set =(X_val, y_val), verbose=False)
+
+            # Append the model to the list
+            model.append(lgbr)
+            logging.info("Training completed successfully")
+            return model
+        except Exception as e:
+            logging.error(f"Error in training the model: {e}")
+            raise e
--- utils/model_train.py
+++ utils/model_train.py
@@ -4,13 +4,33 @@ import logging
 import pandas as pd
 from zenml import step
 
+from src.model import BoostingModels
+
 # Creating a step for model training
 @step
-def train_model(df: pd.DataFrame) -> None:
+def train_model(
+    X_train: pd.DataFrame,
+    X_test: pd.DataFrame,
+    y_train: pd.DataFrame,
+    y_test: pd.DataFrame
+) -> BoostingModels:
     """
     Training the model
 
     Args:
-        df: pandas dataframe
+        X_train (pd.DataFrame): Training data
+        X_test (pd.DataFrame): Testing data
+        y_train (pd.DataFrame): Training labels
+        y_test (pd.DataFrame): Testing labels
+
+    Returns:
+        model (BoostingModels): Trained model
     """
-    pass
+    try:
+        model = None
+        model = BoostingModels()
+        trained_model = model.train(X_train, y_train)
+        return trained_model
+    except Exception as e:
+        logging.error("Error while training model: {}".format(e))
+        raise e
