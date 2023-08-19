7885ebd07e6828e218be8c2d6966b62b2003dac4
Suryansh Gupta <suryanshgupta9933@gmail.com>
2023-08-19 16:45:57 +0530

    updated pipeline

--- run_pipeline.py
+++ run_pipeline.py
@@ -1,5 +1,8 @@
 # Importing Dependencies
 from pipeline.training_pipeline import train_pipeline
 
+from zenml.client import Client
+
 if __name__ == '__main__':
+    print(Client().active_stack.experiment_tracker.get_tracking_uri())
     train_pipeline(data_path='./data/AMAZON_daily.csv')
